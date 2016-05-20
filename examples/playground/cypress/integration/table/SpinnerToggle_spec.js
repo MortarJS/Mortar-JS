1877eb8a50850921220cf414c9fe0f4b3eb0003c
Lea Yousoufov <lyousoufov@fuzzproductions.com>
2016-05-20 14:13:24 -0400

    Complete SpinnerToggle tests

--- examples/playground/cypress/integration/table/SpinnerToggle_spec.js
+++ examples/playground/cypress/integration/table/SpinnerToggle_spec.js
@@ -1,6 +1,6 @@
 describe("Actionable Rows", function () {
 	beforeEach(function () {
-		cy.visit("http://localhost:8080/#/components/table");
+		cy.visit("http://localhost:21356/#/components/table");
 	});
 
 	context("Navigate to Sprinner Table", function () {
@@ -10,6 +10,18 @@ describe("Actionable Rows", function () {
 			});
 
 			cy.get(".table-responsive").find(".spinner-loader").should("exist");
+		});
+
+		it("can navigate to 'spinner' from tab options and toggle spinner off", function () {
+			cy.get(".btn-group").within(function () {
+				cy.contains("spinner").click();
+			});
+
+			cy.get(".mortar-toggle").within(function () {
+				cy.get("label").last().click();
+			})
+
+			cy.get(".table-responsive").find(".table-empty-row").should("exist");
 		});
 	});
 });
