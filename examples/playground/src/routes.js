b77a0132a87ca454516247858eea2c25144485db
Kyle Mendes <kmendes@fuzzproductions.com>
2016-06-06 16:10:04 -0400

    Spacing tweak [ci skip]

--- examples/playground/src/routes.js
+++ examples/playground/src/routes.js
@@ -45,27 +45,27 @@ var ModalContainer = require('./pages/components/global/ModalContainer/ModalCont
  * @TODO: build all these pages
  */
 var Routes = (
-		<Route path="/" component={App}>
-			<IndexRoute component={Dashboard} />
+	<Route path="/" component={App}>
+		<IndexRoute component={Dashboard} />
 
-			<Route path="components/">
-				<Route path="table"          component={Table} />
-				<Route path="tooltip"        component={Tooltip} />
-				<Route path="modalcontainer" component={ModalContainer} />
-				<Route path="buttondrawer"   component={ButtonDrawer} />
-				<Route path="dropdown"       component={DropdownSelect} />
-				<Route path="input"          component={Input} />
-				<Route path="toggle"         component={Toggle} />
-				<Route path="typeaheadinput" component={TypeAheadInput} />
-				<Route path="radio"          component={RadioButtons} />
-				<Route path="checkbox"       component={Checkbox} />
-				<Route path="fileinput"      component={FileInput} />
-				<Route path="textarea"       component={TextArea} />
-			</Route>
-
-
-			<Route path="*" component={Dashboard} />
+		<Route path="components/">
+			<Route path="table"          component={Table} />
+			<Route path="tooltip"        component={Tooltip} />
+			<Route path="modalcontainer" component={ModalContainer} />
+			<Route path="buttondrawer"   component={ButtonDrawer} />
+			<Route path="dropdown"       component={DropdownSelect} />
+			<Route path="input"          component={Input} />
+			<Route path="toggle"         component={Toggle} />
+			<Route path="typeaheadinput" component={TypeAheadInput} />
+			<Route path="radio"          component={RadioButtons} />
+			<Route path="checkbox"       component={Checkbox} />
+			<Route path="fileinput"      component={FileInput} />
+			<Route path="textarea"       component={TextArea} />
 		</Route>
+
+
+		<Route path="*" component={Dashboard} />
+	</Route>
 );
 
 module.exports = Routes;
