804ece9a68b1e8d35d1e8c528e352c0b314f80d4
Lea Yousoufov <lyousoufov@fuzzproductions.com>
2016-06-07 10:49:41 -0400

    [Table] Update Spinner tests

--- examples/playground/cypress/integration/table/SpinnerToggle_spec.js
+++ examples/playground/cypress/integration/table/SpinnerToggle_spec.js
@@ -1,29 +1,24 @@
 describe("Actionable Rows", function () {
 	beforeEach(function () {
 		cy.visit("http://localhost:21356/#/components/table");
+		cy.get(".btn-group").within(function () {
+			cy.contains("spinner").click();
+		});
 	});
 
-	context("Navigate to Sprinner Table", function () {
-		it("can navigate to 'spinner' from tab options", function () {
-			cy.get(".btn-group").within(function () {
-				cy.contains("spinner").click();
-			});
-
+	context("Navigate to Spinner Table", function () {
+		it("has spinner loader", function () {
 			cy.get(".table-responsive")
 				.find(".spinner-loader")
 				.should("exist");
 		});
 
-		it("can navigate to 'spinner' from tab options and toggle spinner off", function () {
-			cy.get(".btn-group").within(function () {
-				cy.contains("spinner").click();
-			});
-
+		it("can toggle spinner off", function () {
 			cy.get(".mortar-toggle").within(function () {
 				cy.get("label")
 					.last()
 					.click();
-			})
+			});
 
 			cy.get(".table-responsive")
 				.find(".table-empty-row")
